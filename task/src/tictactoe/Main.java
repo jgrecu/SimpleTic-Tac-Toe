fe1102248ac5821d28f88482d56e589480945a8b
Jeremy Grecu <jeremy.grecu@sap.com>
2021-06-30 13:23:09 +0100

    JBA - Simple Tic Tac Toe - Simple tic tac toe Stage 2/5

--- task/src/tictactoe/Main.java
+++ task/src/tictactoe/Main.java
@@ -1,13 +1,34 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        char[][] chars = {{'X','O','X'},{'O','X','O'},{'X','X','O'}};
+        final Scanner scanner = new Scanner(System.in);
+
+        char[][] chars = new char[3][3];
+        System.out.println("Enter cells: ");
+        String input = scanner.nextLine();
+
+        int index = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                chars[i][j] = input.charAt(index);
+                index++;
+            }
+        }
+        printBoard(chars);
+    }
+
+    public static void printBoard(char[][] chars) {
+        System.out.println("---------");
         for (char[] row : chars) {
+            System.out.print('|' + " ");
             for (char c : row) {
                 System.out.print(c + " ");
             }
-            System.out.println();
+            System.out.println('|');
         }
+        System.out.println("---------");
     }
 }
